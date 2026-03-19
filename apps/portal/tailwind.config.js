a9595aef43081f5c1c4b39e5b01ca0cc6d011777
unknown <michaelculhane@gmail.com>
2026-03-18 21:41:07 -0400

    fix: resolve @tailwindcss/typography from config file location

--- apps/portal/tailwind.config.js
+++ apps/portal/tailwind.config.js
@@ -21,5 +21,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require(require.resolve('@tailwindcss/typography', { paths: [__dirname] }))],
 }
